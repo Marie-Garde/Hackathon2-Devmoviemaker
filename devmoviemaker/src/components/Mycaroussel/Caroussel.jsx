574a296f8fc8d7b418f57741fefc2a72564b2e9f
Guy de Messimy <gdemessimy@gmail.com>
2021-01-13 22:52:53 +0100

    add css

--- devmoviemaker/src/components/Mycaroussel/Caroussel.jsx
+++ devmoviemaker/src/components/Mycaroussel/Caroussel.jsx
@@ -14,14 +14,14 @@ function Slider () {
   return (
     <div className="App">
       <Carousel breakPoints={breakPoints}>
-        <Item>Rédiger un script/scénario reflétant votre parcours, écrivez-le !</Item>
-        <Item>Les sections doivent contenir une accroche, une présentation, vos formations, vos expériences, vos centres d’intérêt en quelques mots,</Item>
-        <Item>L’étape d’après vous aidera à timer votre scénario en fonction de votre profil</Item>
-        <Item>Choisir un plan statique (pas de gros plan, pas de plan large)</Item>
-        <Item>Privilégier une ambiance lumineuse claire en évitant les néons</Item>
-        <Item>Adopter une posture statique mais garder un dynamisme en n’oubliant pas que 60 % de la communication est visuelle</Item>
-        <Item>Soigner votre tenue vestimentaire (pas de rayures, faites la correspondre aux code de votre profession)</Item>
-        <Item>Faites attention à la qualité audio : pas de bruits extérieurs, articuler !</Item>
+        <Item>Rédiger un script/scénario reflétant votre parcours, <br></br>et écrivez-le !</Item>
+        <Item>Prévoir une accroche, une présentation, vos formations, vos expériences, vos centres d’intérêt en quelques mots</Item>
+        <Item> Notre outil timer vous permettra de calquer les sections de votre scénario sur la vidéo</Item>
+        <Item>Choisir un plan statique et eviter le gros plan et plan large</Item>
+        <Item>Privilégier une ambiance lumineuse et claire en évitant les néons</Item>
+        <Item>Adopter une posture statique mais garder un dynamisme en n’oubliant pas la communication visuelle</Item>
+        <Item>Soigner votre tenue vestimentaire, faites la correspondre aux codes de votre profession, eviter les rayures</Item>
+        <Item>Faites attention à la qualité audio, neutraliser les bruits extérieurs, articuler !</Item>
         <Item>Entraînez-vous jusqu’à trouver le bon équilibre et la bonne fluidité</Item>
         <Item>Vous avez entre 1mn et 1mn30 pour être impactant(e) </Item>
       </Carousel>
--- devmoviemaker/src/components/Mycaroussel/Item.jsx
+++ devmoviemaker/src/components/Mycaroussel/Item.jsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export default styled.div`
   display: flex;
+  height: 35vh;
   justify-content: center;
   align-items: center;
-  height: 250px;
-  width: 100%;
+  text-align: center;
+  padding: 5px 8px 5px 8px;
+  line-height: 2em;
   background-color: #683bb7;
   color: #fff;
-  margin: 15px;
-  font-size: 1em;
+  margin: 5px;
+  font-size: 1.2em;
 `;
