9380ab2cc772be2de5531f5d2918fd492f8e2a21
Thomas Druart <thomas.druart.59@gmail.com>
2021-01-13 23:14:41 +0100

    WIP

--- devmoviemaker/src/App.jsx
+++ devmoviemaker/src/App.jsx
@@ -1,13 +1,33 @@
 import { Reset } from "styled-reset";
 import VerticalLinearStepper from "./components/Stepper";
 import Webcam from "./components/Webcam";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const page = useSelector((state) => state.menuReducer.page);
+  const dispatch = useDispatch();
+
+  const renderPage = () => {
+    if (page === "home") {
+      return <Webcam />;
+    } else if (page === "advices") {
+      return <Webcam />;
+    } else if (page === "time") {
+      return <Webcam />;
+    } else if (page === "record") {
+      return <Webcam />;
+    } else {
+      return <Webcam />;
+    }
+  };
+
   return (
     <div>
       <Reset />
-      <VerticalLinearStepper/>
+      <button onClick={() => dispatch({ type: "RECORD" })}>Mets ta cam!</button>
+      <VerticalLinearStepper />
       <Webcam />
+      {renderPage()}
     </div>
   );
 }
