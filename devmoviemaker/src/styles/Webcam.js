502609cfea5d24aa49b6f6a9ee1485dd7c7fce0b
Thomas Druart <thomas.druart.59@gmail.com>
2021-01-13 15:49:40 +0100

    button style added

--- devmoviemaker/src/styles/Webcam.js
+++ devmoviemaker/src/styles/Webcam.js
@@ -1,3 +1,21 @@
 import styled from "styled-components";
 
-export const Main = styled.div``;
+export const Main = styled.div`
+  button {
+    display: inline-block;
+    padding: 0.7em 1.7em;
+    margin: 0 0.3em 0.3em 0;
+    border-radius: 0.2em;
+    border-style: none;
+    box-sizing: border-box;
+    text-decoration: none;
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    color: #ffffff;
+    background-color: #3369ff;
+    box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17),
+      inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15),
+      inset 0 0 0em 0.05em rgba(255, 255, 255, 0.12);
+    text-align: center;
+  }
+`;
