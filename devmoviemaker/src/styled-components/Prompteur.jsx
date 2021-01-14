723bdd019e7e4a193942b8ed4ca037898406120b
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2021-01-14 23:08:15 +0100

    Style Card left

--- devmoviemaker/src/styled-components/Prompteur.jsx
+++ devmoviemaker/src/styled-components/Prompteur.jsx
@@ -5,41 +5,47 @@ export const FlexBox = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
+  padding-left: 28.7vh;
 `;
 
 export const BebeFlex1 = styled.div`
   width: 20vw;
   height: 20vh;
-  background-color: #c4c4c4;
+  background-color: #4575a6;
+  border: 0.2rem solid;
 `;
 
 export const BebeFlex2 = styled.div`
   width: 20vw;
   height: 20vh;
-  background-color: #363030;
+  background-color: #2b4866;
+  border: 0.2rem solid;
 `;
 
 export const BebeFlex3 = styled.div`
   width: 20vw;
   height: 20vh;
-  background-color: #110c0c;
+  background-color: #2b4866;
+  border: 0.2rem solid;
 `;
 
 export const BebeFlex4 = styled.div`
   width: 20vw;
   height: 20vh;
-  background-color: #4b3636;
+  background-color: #2b4866;
+  border: 0.2rem solid;
 `;
 
 export const BebeFlex5 = styled.div`
   width: 20vw;
   height: 20vh;
-  background-color: #941a1a;
+  background-color: #2b4866;
+  border: 0.2rem solid;
 `;
 
 export const H2Flex = styled.div`
-color : white;
-text-align : center;
-padding-top : 2vh;
-font-size : 1.8rem;
-`
+  color: white;
+  text-align: center;
+  padding-top: 2vh;
+  font-size: 1.5rem;
+`;
