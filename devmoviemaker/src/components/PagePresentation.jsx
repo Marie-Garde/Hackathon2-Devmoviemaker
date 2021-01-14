37b34f2688d7af3672b66f4b5dff6fdd61da2cb7
Thomas Druart <thomas.druart.59@gmail.com>
2021-01-14 15:11:41 +0100

    title changed

--- devmoviemaker/src/components/PagePresentation.jsx
+++ devmoviemaker/src/components/PagePresentation.jsx
@@ -3,8 +3,8 @@ import { DivMain, H1, P } from "../styled-components/PagePresentation";
 export default function PagePresentation() {
   return (
     <DivMain>
-      <H1>Créer le CV vidéo idéal</H1>
-      <P>Lorem ipsum dolor sit amet bal bla bla bla bla</P>
+      <H1>Crée le CV vidéo idéal</H1>
+      <P>Démarrer</P>
     </DivMain>
   );
 }
