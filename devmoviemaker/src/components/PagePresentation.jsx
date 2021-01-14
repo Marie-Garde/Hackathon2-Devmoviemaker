111881884fca4da8cef717860f98fc4e6623bcc2
Thomas Druart <thomas.druart.59@gmail.com>
2021-01-14 15:33:11 +0100

    text changed on the presentation page

--- devmoviemaker/src/components/PagePresentation.jsx
+++ devmoviemaker/src/components/PagePresentation.jsx
@@ -3,8 +3,8 @@ import { DivMain, H1, P } from "../styled-components/PagePresentation";
 export default function PagePresentation() {
   return (
     <DivMain>
-      <H1>Crée le CV vidéo idéal</H1>
-      <P>Démarrer</P>
+      <H1>Créer le CV vidéo idéal</H1>
+      <P>Dévoilez vos talents de manière originale</P>
     </DivMain>
   );
 }
